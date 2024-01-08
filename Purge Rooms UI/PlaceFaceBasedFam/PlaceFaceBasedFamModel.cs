2dc50493e55e0212ce5a10ef4d05b6222402d0db
Ivo Petrov <48355182+ivaylo-matov@users.noreply.github.com>
2024-01-08 13:02:49 +0000

    face based fam

---
+++ Purge Rooms UI/PlaceFaceBasedFam/PlaceFaceBasedFamModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purge_Rooms_UI.PlaceFaceBasedFam
+{
+    internal class PlaceFaceBasedFamModel
+    {
+    }
+}
