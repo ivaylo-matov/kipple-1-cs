579b89e93df4d4761c1bf9700a16524f0d2e2a38
Ivo Petrov <48355182+ivaylo-matov@users.noreply.github.com>
2024-01-13 16:43:42 +0000

    progress

--- Purge Rooms UI/Bimorph/BimorphViewModel.cs
+++ Purge Rooms UI/Bimorph/BimorphViewModel.cs
@@ -6,16 +6,16 @@ namespace Purge_Rooms_UI.Bimorph
 {
     public class BimorphViewModel : ViewModelBase
     {
-        public BimorphModel Model { get; set; }
-        public RelayCommand<Window> Process { get; set; }
+        public BimorphModel Model { get; set; }             // property to hold the model
+        public RelayCommand<Window> Process { get; set; }   // property ho hold the command/process
 
-        public BimorphViewModel(BimorphModel model)
+        public BimorphViewModel(BimorphModel model)         // constructor for the view-model
         {
             Model = model;
             Process = new RelayCommand<Window>(OnExecuteRun);
         }
 
-        private void OnExecuteRun(Window win)
+        private void OnExecuteRun(Window win)               // method called when button is clicked
         {
             Model.MainCode();
             win.Close();
